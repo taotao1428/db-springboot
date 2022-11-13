1a415922670d6d33dc31da84f220ab4b92897429
hewutao <1320238094@qq.com>
2022-11-13 14:13:52 +0800

    init

---
+++ src/main/java/com/hewutao/db/DbSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.hewutao.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hewutao/db/DbSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hewutao.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
